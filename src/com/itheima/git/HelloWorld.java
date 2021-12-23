83209085e7134118959d8e00daf56fbe7e484188
Sintredial <xxggxxgg2010@outlook.com>
2021-12-23 21:47:50 +0800

    Master分支, 新增代码-222

--- src/com/itheima/git/HelloWorld.java
+++ src/com/itheima/git/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("HelloWorld");
+
+        System.out.println("我是Master中新增的代码2222");
     }
 }
