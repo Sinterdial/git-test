c3540901f549c00a1b0724516f2bd7c8c5e0ba0f
Sintredial <xxggxxgg2010@outlook.com>
2021-12-23 21:47:13 +0800

    Dev01分支, 新增代码-111

--- src/com/itheima/git/HelloWorld.java
+++ src/com/itheima/git/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("HelloWorld");
+
+        System.out.println("我是dev分支中添加的第一条代码");
     }
 }
